f80d228464e7d24e7b5694f311f49604d32d3505
Emile Vargas <130794679+emivargxs@users.noreply.github.com>
2023-12-14 23:37:51 -0300

    Add files via upload

---
+++ aula3.cpp
@@ -0,0 +1,19 @@
+#include <iostream> //de entrada e saida, comandos básicos de entrada e saída.
+ 
+ using namespace std; //std outro namespace, namespace: aonde estao as bibliotecas que vamos trabalhar, organização de pastas;
+
+//corpo do programa:
+
+int main(){
+
+    cout << "Emile Vargas Bordin\n";        //imprime conteudo na tela //endl quebra linha
+
+    
+    //cout << "Emile Vargas Bordin\n">>endl;        
+
+    
+  //system("pause"); //Espera uma tecla para continuar 
+
+
+    return 0;
+}
