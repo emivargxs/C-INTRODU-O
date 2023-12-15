c89eea7d1650fccbe3129f974ac619c52849f3d2
Emile Vargas <130794679+emivargxs@users.noreply.github.com>
2023-12-15 01:02:50 -0300

    Add files via upload

---
+++ Aula_5 - Declarações múltiplas de variáveis, Constantes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415 
+#define canal cout << "Emile Vargas";
+
+//Constante: nao pode mudar FIXO.
+
+
+
+int main(){
+
+    int vidas = 3, tiros = 500, life = 100;
+
+    cout << pi << "\n\n";
+    //chamar a constante canal
+    canal 
+
+
+    return 0;
+}
