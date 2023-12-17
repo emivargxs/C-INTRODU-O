60e04d2b8816128ab7d5e3adc1280c087ca20058
Emile Vargas <130794679+emivargxs@users.noreply.github.com>
2023-12-17 14:29:41 -0300

    Add files via upload

---
+++ aula_8 -  Invertendo valores de variáveis.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //Valor normal
+    int num = 10;
+    cout << num << "\n\n";
+
+    //Invertendo
+    num = num * -1; //Inverte o valor
+    cout << num << "\n\n"; // USANDO ISSO ELE VAI INVERTER O VALOR E GRAVAR
+ 
+    //Outra maneira de inverter
+    num= - num; //Inverte o valor
+
+    return 0;
+}
