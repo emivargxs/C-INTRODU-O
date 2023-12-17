c81bd11562deedaa0e67971f8e1a251472a1823b
Emile Vargas <130794679+emivargxs@users.noreply.github.com>
2023-12-17 14:14:47 -0300

    Add files via upload

---
+++ aula_7 -  Incremento e Decremento de Variáveis, Operadores pós e pré-fixados.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n1, n2;
+
+    //Inicializando com os valores:
+    n1 = 0;
+    n2 = 10;
+
+    //Decremento e Incremento
+
+    cout << n1 << "\n\n"; //Antes do incremento
+
+    //Adicionar +1 ao valor de n1
+    n1 = n1 + 1; 
+
+    //Depois do incremento
+    cout << n1 << "\n\n"; // n1 = 0 + 1 = 1
+
+    //Exemplos Incremento:
+
+    n1 = n1 + 2; 
+    cout << n1 << "\n\n";
+    n1 = n1 + 3; 
+    cout << n1 << "\n\n";
+    n1 = n1 + 4; 
+    cout << n1 << "\n\n";
+    n1 = n1 + 5; 
+    cout << n1 << "\n\n";
+
+
+    return 0;
+
+}
