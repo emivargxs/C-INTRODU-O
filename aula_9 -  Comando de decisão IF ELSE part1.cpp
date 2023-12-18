c43cbc7c961689298b244465c2b8a57a515c3b8a
Emile Vargas <130794679+emivargxs@users.noreply.github.com>
2023-12-17 23:07:03 -0300

    Add files via upload

---
+++ aula_9 -  Comando de decisão IF ELSE part1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num1 = 10;
+    int num2 = 50;
+
+    char opc = 's';
+
+    if(num1 > num2){
+        cout << "Verdade.";
+    } else {
+        cout << "Falso.";
+
+    }
+
+    //teste2
+    int sol = 1;
+    
+    if(sol){ //Verdadeiro 1
+        cout << "Vou ao clube.";
+    } else{ //Falso 0
+        cout << "vou ao cinema.";
+        
+    }
+
+    return 0;
+}
